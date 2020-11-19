0b3d6d976bafeba4962099592b4434ae998f1d39
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2020-11-19 09:09:49 +0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+console.log("tabs");
+console.log("modal");
+
+
+$(function(){
+    $(".click__phone").click(function(){
+        $(".header__num").slideToggle(1000);
+    });
+});
+
+
+
+$(function(){
+    var input = document.querySelectorAll("#result");
+    document.querySelectorAll("#plus").forEach((elem,index)=>{
+        elem.onclick=e=>{
+            input[index].value = Number(input[index].value) + 1;
+        }
+    });
+
+    document.querySelectorAll("#minus").forEach((elem,index)=>{
+        elem.onclick=e=>{
+            input[index].value = Number(input[index].value) - 1;
+        }
+    });
+});
